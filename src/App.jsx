74819e823d949fe99f8355cc36e58147c8c23e94
jose86b <jdominguezgordill@gmail.com>
2021-07-07 22:27:02 -0600

    hola

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,55 @@
 import shortid from "shortid";
+import React from "react"
+function Copy() {
+  
+  //ESTADOS
+  const [tarea, setTarea] = React.useState('')
+  const [tareas, setTareas] = React.useState([])
+  const [modoEdicion,setModoEdicion] = React.useState(false)
+  const [id, setID] =React.useState(0);
+  const [ mensajeErrror, setMensajeError ] = React.useState(false)
+  //FUNCION PARA AGREGAR TAREA
+  const agregarTarea = e => {
+    e.preventDefault()
+    if (!tarea.trim()) {
+      setMensajeError(true)
+      return
+    }
+    setTareas([
+      ...tareas,
+      { id:shortid.generate(), nombreTarea:tarea }
+    ])
+    setTarea("")
+    setMensajeError(false)
 
+  }
 
-function App() {
+  //FUNCION PARA ELIMINAR LA TAREA
+  const eliminarTarea = (id) => {
+    console.log(id)
+    const arrayFiltrado = tareas.filter(item => item.id !== id)
+    setTareas(arrayFiltrado)
+  }
+  const cambiarEstado = (item) => {
+    setModoEdicion(true)
+    setTarea(item.nombreTarea);
+    setID(item.id)
+  }
+  //Ediccion 
+  const editarTarea = (e) => {
+    e.preventDefault()
+    if (!tarea.trim()) {
+      setMensajeError(true);
+      return
+    }
+    const arrayEditado = tareas.map(
+      item => item.id === id ? { id: id, nombreTarea: tarea } : item
+    )
+    setTareas(arrayEditado)
+    setTarea("")
+    setModoEdicion(false)
+    setMensajeError(false)
+  }
   return (
     <div className="container mt-5">
       <h1 className="text-center">CRUD</h1>
@@ -9,19 +57,57 @@ function App() {
         <div className="col-8">
           <h4 className="text-center">Lista de tareas</h4>
           <ul className="list-group">
-            <li className="list-group-item">
-              <span className="lead">Nombre de la tarea</span>
-              <button className="btn btn-danger btn-sm float-end mx-2">Eliminar</button>
-              <button className="btn btn-warning btn-sm float-end mx-2">Editar</button>
-            </li>
+            {
+              tareas.length === 0 ? (
+                <li className="list-group-item">
+                  <span className="lead">No existen tareas</span>
+                </li>
+              ) :
+              (
+                tareas.map(item => (
+                  <li className="list-group-item" key={item.id}>
+                    <span className="lead">{ item.nombreTarea }</span>
+                    <button
+                      className="btn btn-danger btn-sm float-end mx-2"
+                      onClick={() => eliminarTarea(item.id)}
+                    >
+                      Eliminar
+                    </button>
+                    <button
+                      className="btn btn-warning btn-sm float-end mx-2"
+                      onClick={ () => cambiarEstado(item) }
+                    >
+                      Editar
+                    </button>
+                  </li>
+                ))
+              )
+            }
           </ul>
         </div>
         <div className="col-4">
-          <h4 className="text-center">Registrar</h4>
+          <h4 className="text-center"> { modoEdicion ? 'Editar' : 'Registrar' }</h4>
+          {
+            mensajeErrror ? (
+              <h6 className="text-center text-danger">NO SE PERMITEN CAMPOS VASIOS</h6>
+            ) : ( <h2></h2> )
+          }
           <h4 className="text-center">
-            <form>
-              <input type="text" className="form-control mb-2" placeholder="Ingrese tarea" />
-              <button className="btn btn-dark btn-block" type="submit">Agregar</button>
+            <form onSubmit={ modoEdicion ? editarTarea : agregarTarea }>
+              <input
+                type="text"
+                className="form-control mb-2"
+                placeholder="Ingrese tarea"
+                onChange={e => setTarea(e.target.value)}
+                value={tarea}
+              />
+              {
+                modoEdicion ? (
+                  <button className="btn btn-warning btn-block" type="submit">Editar</button>
+                ): (
+                  <button className="btn btn-dark btn-block" type="submit">Agregar</button>
+                )
+              }
             </form>
           </h4>
         </div>
@@ -30,4 +116,4 @@ function App() {
   );
 }
 
-export default App;
+export default Copy;
